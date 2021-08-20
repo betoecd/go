cf2f8a7b9350de87457f1461a5fff29ba01c8ab4
Eberton Dias <eberton.bits@gmail.com>
2021-08-20 16:58:46 -0300

    ✨ Math module

---
+++ math.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Soma(10, 10))
+}
+
+func Soma(x float32, z float32) float32 {
+	return x + z
+}
---
+++ math_test.go
@@ -0,0 +1,3 @@
+func TestSoma(t *testing) {
+
+}
